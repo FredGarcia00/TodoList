cbbc587c8251feddca1dc869509f270d756e3b2e
Fred Garcia <Fred@Garcia.com>
2020-07-26 12:51:16 -0700

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function About() {
+    return (
+        <React.Fragment>
+            <h1>about</h1>
+            <p>this is a TodoList app. It is part of a React crash course</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
